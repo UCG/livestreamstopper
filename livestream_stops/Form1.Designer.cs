c1a91ca9d040a85eb92456021a5529a0fcec0c0e
sophie pebworth <sophie.pebworth@gmail.com>
2020-01-12 18:22:02 -0500

    Error Checks, UI updated, input no longer hard-coded

--- livestream_stops/Form1.Designer.cs
+++ livestream_stops/Form1.Designer.cs
@@ -28,64 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.TimeDelay = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SubmitButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ButtonClick1 = new System.Windows.Forms.TextBox();
+            this.ButtonClick2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // TextBox1
+            // TimeDelay
             // 
-            this.TextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.TimeDelay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TextBox1.Location = new System.Drawing.Point(198, 84);
-            this.TextBox1.Name = "TextBox1";
-            this.TextBox1.Size = new System.Drawing.Size(100, 20);
-            this.TextBox1.TabIndex = 0;
+            this.TimeDelay.Location = new System.Drawing.Point(74, 93);
+            this.TimeDelay.Name = "TimeDelay";
+            this.TimeDelay.Size = new System.Drawing.Size(83, 20);
+            this.TimeDelay.TabIndex = 0;
+            this.TimeDelay.Text = "5";
+            this.TimeDelay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(179, 50);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.Size = new System.Drawing.Size(177, 16);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Enter Time for FTB (seconds):";
-           // this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Delay Time (in seconds)";
             // 
             // SubmitButton
             // 
             this.SubmitButton.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.SubmitButton.Location = new System.Drawing.Point(198, 125);
+            this.SubmitButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.SubmitButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(309, 169);
             this.SubmitButton.Name = "SubmitButton";
-            this.SubmitButton.Size = new System.Drawing.Size(100, 23);
+            this.SubmitButton.Size = new System.Drawing.Size(129, 37);
             this.SubmitButton.TabIndex = 4;
             this.SubmitButton.Text = "Enter";
             this.SubmitButton.UseVisualStyleBackColor = true;
             this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 29);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Button Click #1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(534, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Button Click #2";
+            // 
+            // ButtonClick1
+            // 
+            this.ButtonClick1.Location = new System.Drawing.Point(320, 93);
+            this.ButtonClick1.Name = "ButtonClick1";
+            this.ButtonClick1.Size = new System.Drawing.Size(100, 20);
+            this.ButtonClick1.TabIndex = 8;
+            this.ButtonClick1.Text = "INSERT";
+            this.ButtonClick1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ButtonClick2
+            // 
+            this.ButtonClick2.Location = new System.Drawing.Point(537, 93);
+            this.ButtonClick2.Name = "ButtonClick2";
+            this.ButtonClick2.Size = new System.Drawing.Size(106, 20);
+            this.ButtonClick2.TabIndex = 9;
+            this.ButtonClick2.Text = "END";
+            this.ButtonClick2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(270, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "LiveStream Stopper";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 234);
-            this.ControlBox = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(729, 228);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ButtonClick2);
+            this.Controls.Add(this.ButtonClick1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.SubmitButton);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.TimeDelay);
             this.Name = "Form1";
             this.Text = "Livestream Stopper";
-            //this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-           // this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TimeDelay;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ButtonClick1;
+        private System.Windows.Forms.TextBox ButtonClick2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- livestream_stops/Form1.cs
+++ livestream_stops/Form1.cs
@@ -14,11 +14,13 @@ namespace livestream_stops
     public partial class Form1 : Form
     {
         static Timer myTimer = new System.Windows.Forms.Timer();
+        //private EventHandler check_keypress_simulation;
 
         public Form1()
         {
             InitializeComponent();
             this.KeyPreview = true;
+            //check_keypress_simulation += new EventHandler(check_keypress_simulation);
             //SubmitButton.Click += new EventHandler(SubmitButton_Click);
 
         }
@@ -28,47 +30,98 @@ namespace livestream_stops
         //It checks for proper numerical input and waits that amount of time to show pop up box
         async void SubmitButton_Click(object sender, EventArgs e)
         {
-            //checks TextBox input
-            string time_input = TextBox1.Text;
-            if (time_input.Trim() == "")
+            //gets user input
+            string time_input = TimeDelay.Text;
+            string button1 = "{" + ButtonClick1.Text + "}";
+            string button2 = "{" + ButtonClick2.Text + "}";
+
+            //check input
+            if (!check_valid_time_input(time_input))
             {
                 MessageBox.Show("Please enter a valid number");
+                TimeDelay.Text = "";
                 return;
             }
-            for (int i = 0; i < time_input.Length; i++)
+            if (!check_valid_button_input(button1))
             {
-                if (!char.IsNumber(time_input[i]))
-                {
-                    MessageBox.Show("Please enter a valid number");
-                    TextBox1.Text = "";
-                    return;
-                }
-
+                MessageBox.Show("Incorrect input for Button Click #1, please try again");
+                ButtonClick1.Text = "";
+                return;
+            }
+            if (!check_valid_button_input(button2))
+            {
+                MessageBox.Show("Incorrect input for Button Click #2, please try again");
+                ButtonClick2.Text = "";
+                return;
             }
 
             //sets timer and displays message after that set time     
             int wait_time = Int32.Parse(time_input) * 1000;
-            myTimer.Interval = wait_time;
-            myTimer.Enabled = true;
-
             await Task.Delay(wait_time);
-           // MessageBox.Show("wait time: " + TextBox1.Text + " seconds");
-            //SubmitButton.Enabled = true;
-
-            //simulate FTB keypress
-            SendKeys.Send("{INSERT}");
-            MessageBox.Show("pressed Insert Key after " + TextBox1.Text + " seconds");
+            
+            //simulate FTB press
+            SendKeys.Send(button1);
+            MessageBox.Show("pressed " + button1);
 
             //wait 30 seconds
             await Task.Delay(30000);
 
             //simulate stream keypress
-            SendKeys.Send("{END}");
-            MessageBox.Show("Pressed Enter Key after 30 seconds");
-
+            SendKeys.Send(button2);
+            MessageBox.Show("Pressed" + button2);
+           
         }
 
+        private bool check_valid_time_input(string input)
+        {
+            if (input.Trim() == "")
+            {
+                return false;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsNumber(input[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool check_valid_button_input(string input)
+        {
+            List<string> sendkeysignals = new List<string> {"{END}", "{INSERT}"};
+            if (sendkeysignals.Contains(input))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //function that allows form to submit by pressing "enter"
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SubmitButton.PerformClick();
+            }
+
+            else
+            {
+                if(e.KeyCode == Keys.Insert)
+                {
+                    MessageBox.Show("Clicked Insert");
+                }
+                if(e.KeyCode == Keys.End)
+                {
+                    MessageBox.Show("Clicked Exit");
+                }
+            }
+        }
+
+        
     }
+ 
 
 
 }
